1f40307aa437f3b4ee3f719a0dcb28b832ab3243
aijazkhan81 <aijazkhan81@gmail.com>
2016-07-22 23:10:38 -0600

    Added a load more button on the homepage and it hides when all the listings have loaded.

--- components/controller/mainCtrl.js
+++ components/controller/mainCtrl.js
@@ -7,5 +7,20 @@ app.controller('mainCtrl', function($scope, $http, readJson){
 	});
 
 	$scope.letterLimit =  31;
+	$scope.onlySix = 6;
+	$scope.hideLoadMore = false;
+	var listingLength;
 
+	// This is an onclick function
+	$scope.loadMore = function(){
+		$scope.onlySix = $scope.onlySix + 6;
+
+		readJson.readJsonfun().then(function(data){
+			listingLength = data.data.length -1;
+		});
+
+		if($scope.onlySix >= listingLength){
+			$scope.hideLoadMore = true;
+		}
+	}
 });
